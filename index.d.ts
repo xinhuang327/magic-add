ca227f76742022d726854d02de7ba90f992a3127
Adrian Huang <xinhuang327@gmail.com>
2017-02-11 09:23:51 +0800

    use typescript

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,8 @@
-declare module "magic-add" {
-	namespace magic_add { }
-	function magic_add(numberA: any, numberB: any): any;
-	export = magic_add;
-}
+/**
+ * Perform a magical add operation
+ * @param {number} numberA
+ * @param {number} numberB
+ * @return {number}
+ */
+declare function magicAdd(numberA: number, numberB: number): number;
+export = magicAdd;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-"use strict";
+
 /**
  * Perform a magical add operation
  * @param {number} numberA
  * @param {number} numberB
  * @return {number}
  */
-function magicAdd(numberA, numberB) {
-    return numberA + numberB;
+function magicAdd(numberA: number, numberB: number): number {
+	return numberA + numberB
 }
-module.exports = magicAdd;
+export = magicAdd
